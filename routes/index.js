fe1f91acb3ce35296115ab50f8c5743b47888dc8
shubhi <shubhipradhan94@yahoo.com>
2019-03-29 13:29:28 +0530

    added contact page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'index' });
+    res.render('index', { title: 'index' });
+});
+router.get('/contact', function(req, res, next) {
+    res.render('contact', { title: 'index' });
 });
 
 module.exports = router;
