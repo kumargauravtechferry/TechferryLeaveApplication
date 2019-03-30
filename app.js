d24fa79ab2347914edacf75a76be68ef31f7c2b3
jayant23 <jayant9583@gmail.com>
2019-03-29 17:23:18 -0700

    login changes

--- app.js
+++ app.js
@@ -4,14 +4,86 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var hbs = require('hbs');
-
+var mysql = require('mysql');
+var session = require('express-session');
+var bodyParser = require('body-parser');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var loginRouter = require('./routes/login');
 
+//#region  login method
+
+// put the credentials to coonect with database
+var connection = mysql.createConnection({
+	host     : 'localhost',
+	user     : 'root',
+	password : '12345',
+	database : 'nodelogin'
+});
+
+// Express is what we'll use for our web applications, this includes packages@
+//  useful in web development, such as sessions and handling HTTP requests, to initialize it we can do:
+
 var app = express();
 
-// view engine setup
+
+//We now need to let Express know we'll be using some of its packages:
+
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
+
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
+app.get('/login', function(request, response) {
+	response.render('./login.hbs');
+});
+
+// check the user credentials for login
+
+app.post('/auth', function(request, response) {
+
+	var username = request.body.username;
+	var password = request.body.password;
+	if (username && password) {
+		connection.query('SELECT * FROM accounts WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+			if (results.length > 0) {
+				request.session.loggedin = true;
+				request.session.username = username;
+		response.send('Welcome '+request.session.username);
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
+});
+
+
+app.get('/users.hbs', function(request, response) {
+
+	if (request.session.loggedin) {
+		response.render('/users.hbs', {
+			username: request.session.username
+		});
+		//response.send('Welcome back, <h1>' +  + '</h1>!');
+	} else {
+		response.send('Please login to view this page!');
+	}
+	response.end();
+});
+
+
+//#endregion
+
+// // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
